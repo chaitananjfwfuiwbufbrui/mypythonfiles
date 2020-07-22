4ade57e9246109cc64a1a7fcecc2874000ab92b3
chaitananjfwfuiwbufbrui <59876819+chaitananjfwfuiwbufbrui@users.noreply.github.com>
2020-07-22 08:08:56 +0530

    3 hours code
    one line fasak

--- app.py
+++ app.py
@@ -1,17 +1,2 @@
-
 def digital_root(n):
-    a = list(map(int, ' '.join(str(n)).split()))
-    z = 0
-    for i in a:
-        z = z + i
-
-    if z > 10:
-        return digital_root(z)
-
-    else:
-        return z
-n = int(input("enter 6thge number"))
-
-
-
-
+    return n if n < 10 else digital_root(sum(map(int,str(n))))
